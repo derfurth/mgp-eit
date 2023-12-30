83f7b2aa933ced90344d446aee08bc1470a17448
Florian d'Erfurth <florian@derfurth.com>
2023-12-30 16:35:39 +0100

    Rends les démarches supprimées inaccessibles

--- back/supabase/migrations/20231230150852_suppression.sql
+++ back/supabase/migrations/20231230150852_suppression.sql
@@ -5,4 +5,16 @@ alter table demarche
 
 comment on column demarche.deleted is 'Vrai si la démarche est supprimée.';
 
+create or replace function my_demarches() returns setof demarche
+    language sql
+as
+$$
+select demarche
+from demarche
+where not deleted
+  and (is_administrateur()
+    or is_animateur_of(id)
+    or is_co_animateur_of_demarche(id));
+$$;
+
 commit;
