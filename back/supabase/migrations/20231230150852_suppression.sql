12c52050e0531e65ee36e26e393bcbd43495e538
Florian d'Erfurth <florian@derfurth.com>
2023-12-31 15:56:22 +0100

    Permet de supprimer les fiches à partir des flux et de filtrer les fiches en temps réel

--- back/supabase/migrations/20231230150852_suppression.sql
+++ back/supabase/migrations/20231230150852_suppression.sql
@@ -17,4 +17,14 @@ where not deleted
     or is_co_animateur_of_demarche(id));
 $$;
 
+alter table fiche
+    drop constraint fiche_flux_id_fkey;
+
+alter table fiche
+    add foreign key (flux_id) references flux
+        on delete cascade;
+
+alter table fiche add column realtime_contact_filter text generated always as ( demarche_id::text || atelier_id::text || contact_id::text ) stored;
+comment on column fiche.realtime_contact_filter is 'Permet de filtrer sur plusieurs critères pour afficher les évolutions des fiches d''un contact en temps réel.';
+
 commit;
