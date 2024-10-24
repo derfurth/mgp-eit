c5c352f9964881457ebc6c35af6053b215b6ec23
Florian d'Erfurth <florian@derfurth.com>
2024-10-24 20:47:20 +0200

    Modèle du module de planification des rencontres.

---
+++ client/lib/models/schedule.dart
@@ -0,0 +1,156 @@
+import 'package:collection/collection.dart';
+
+/// The configuration for the schedule.
+///
+/// Participants will meet around tables and will change table at each turn, the
+/// schedule have a maximum of [turnCount].
+///
+/// The number of available tables is [tableCount] and the maximum of participants
+/// per table is [tableSeatCount].
+class ScheduleConfiguration {
+  final int turnCount;
+  final int tableCount;
+  final int tableSeatCount;
+
+  ScheduleConfiguration({
+    required this.turnCount,
+    required this.tableCount,
+    required this.tableSeatCount,
+  });
+}
+
+/// A simplified card, with the necessary information to build a schedule.
+///
+/// It represent the interest of a [participant] for a given [ressource].
+/// [offre] means the [participant] is offering the [ressource], while [besoin]
+/// means the [participant] needs the [ressource].
+///
+/// [lien] is an optional unique id, where a card is filled, other participant
+/// may add a the same [lien] to their card if they want to be on the table.
+class ScheduleCard {
+  final String participant;
+  final String ressource;
+  final bool offre;
+  final String? lien;
+
+  bool get besoin => !offre;
+
+  ScheduleCard({
+    required this.participant,
+    required this.ressource,
+    required this.offre,
+    this.lien,
+  });
+}
+
+/// The table where [participants] will gather to talk about [ressources].
+///
+/// Usually only one ressource will be the subject of conversation, but in
+/// the case of [Card]s with lien, [ressources] may contain more than one
+/// element.
+class Table {
+  final Set<String> participants;
+  final Set<String> ressources;
+
+  Table({required this.participants, required this.ressources});
+
+  @override
+  String toString() =>
+      '${participants.length} participants: ${participants.join(', ')} on ${ressources.join(', ')}';
+}
+
+/// The turn is a list of [tables] where [participants] will discuss.
+///
+/// A participant can only be at one table at a time.
+class Turn {
+  final tables = <Table>[];
+
+  @override
+  String toString() => '${tables.length} tables:\n${tables.join('\n')}';
+}
+
+/// The schedule, provides list of [turns] after [compute] have been run.
+///
+/// Takes a [configuration] and [cards], computes [tables] and [turns]
+class Schedule {
+  final ScheduleConfiguration configuration;
+  final List<ScheduleCard> cards;
+  late final List<String> participants;
+  late final List<String> ressources;
+
+  final tables = <Table>[];
+  final turns = <Turn>[];
+
+  Schedule({required this.configuration, required this.cards}) {
+    participants = cards.map((s) => s.participant).toSet().toList();
+    ressources = cards.map((s) => s.ressource).toSet().toList();
+  }
+
+  Future<void> compute() async {
+    // Group participants by resource.
+    for (final ressource in ressources) {
+      final participants = cards
+          .where((card) => card.ressource == ressource)
+          .map((card) => card.participant)
+          .toSet();
+
+      // Our participants can all fit around a table.
+      if (participants.length > 1 &&
+          participants.length <= configuration.tableSeatCount) {
+        tables.add(Table(participants: participants, ressources: {ressource}));
+      }
+
+      // They do not fit around a table so we need to split them around.
+      else {
+        int seatCount = configuration.tableSeatCount;
+        while (participants.length % seatCount == 1 && seatCount > 1) {
+          seatCount--;
+        }
+        tables
+            .addAll(participants.slices(seatCount).map((participants) => Table(
+                  participants: participants.toSet(),
+                  ressources: {ressource},
+                )));
+      }
+    }
+
+    // Merge tables with the same participants.
+    for (final table in tables) {
+      final duplicates = tables.where((other) =>
+          other != table && other.participants == table.participants);
+      for (final duplicate in duplicates) {
+        table.ressources.addAll(duplicate.ressources);
+        tables.remove(duplicate);
+      }
+    }
+
+    // Sort tables by the number of participants in descending order.
+    tables
+        .sort((a, b) => b.participants.length.compareTo(a.participants.length));
+
+    // Build as many turns as tables.
+    final turns = [for (var i = 0; i < tables.length; i++) Turn()];
+
+    for (final table in tables) {
+      // Find a turn to add the table
+      turns
+          .firstWhere((turn) =>
+              // - where all tables are not assigned
+              turn.tables.length < configuration.tableCount &&
+              // - no tables have a participant from our table
+              !turn.tables.any((other) => other.participants
+                  .intersection(table.participants)
+                  .isNotEmpty))
+          .tables
+          .add(table);
+      // sort turns to keep empty turns at the end.
+      turns.sort((a, b) => b.tables.length.compareTo(a.tables.length));
+    }
+
+    // Keep the turns with the most tables.
+    this.turns.addAll(turns.take(configuration.tableCount));
+  }
+
+  @override
+  String toString() => '${turns.length} turns:\n${turns.join('\n\n')}';
+}
---
+++ client/test/schedule_test.dart
@@ -0,0 +1,47 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mgp_client/models/faked.dart';
+import 'package:mgp_client/models/schedule.dart';
+
+Future<void> main() async {
+  test('Compute a simple schedule', () async {
+    final schedule = Schedule(
+      cards: [
+        ScheduleCard(participant: 'Alice', ressource: 'Stone', offre: true),
+        ScheduleCard(participant: 'Alice', ressource: 'Wood', offre: true),
+        ScheduleCard(participant: 'Bob', ressource: 'Wood', offre: false),
+        ScheduleCard(participant: 'Clara', ressource: 'Stone', offre: false),
+      ],
+      configuration:
+          ScheduleConfiguration(turnCount: 2, tableCount: 2, tableSeatCount: 2),
+    );
+    await schedule.compute();
+    expect(schedule.turns, isNotEmpty);
+    print(schedule);
+  });
+
+  test('Compute a random schedule', () async {
+    final faker = Faker();
+    final ressources = ['Stone', 'Wood', 'Iron', 'Gold'];
+    final participants = List.generate(20, (_) => faker.person.name());
+    final cards = List.generate(
+        40,
+        (_) => ScheduleCard(
+              participant: participants.random(),
+              ressource: ressources.random(),
+              offre: faker.randomGenerator.boolean(),
+            ));
+
+    final schedule = Schedule(
+      cards: cards, // Generates 10 random ScheduleCards
+      configuration: ScheduleConfiguration(
+        turnCount: 4,
+        tableCount: 4,
+        tableSeatCount: 6,
+      ),
+    );
+    await schedule.compute();
+    expect(schedule.turns, isNotEmpty);
+    print(schedule);
+  });
+}
